a44f35ff702aa76b1a0d282d745ce01610c75e0e
Rajia Rani <rajiarani.syal@gmail.com>
2024-03-27 13:48:44 -0500

    added new file

---
+++ 3.NODE/Backend-1(NodeJS)/math.js
@@ -0,0 +1,28 @@
+const sum = ( a, b) => a + b ;
+const mul = (a, b) => a * b ;
+const div = ( a, b) => a / b ;
+const sub = (a, b) => a - b ;
+const pi = 3.14;
+const g = 9.8;
+
+
+module.exports ={
+        sum : sum,
+        mul : mul,
+        div : div,
+        sub: sub,
+        pi : pi,
+        g : g,
+    };
+
+
+// let obj = {
+//     sum : sum,
+//     mul : mul,
+//     div : div,
+//     sub: sub,
+//     pi : pi,
+//     g : g,
+// };
+
+// module.exports = obj ;
--- 3.NODE/Backend-1(NodeJS)/script.js
+++ 3.NODE/Backend-1(NodeJS)/script.js
@@ -3,7 +3,15 @@
 //     console.log("hello", i);
 // }
 //console.log(process.argv);
-let args = process.argv;
-for(let i =2 ; i<args.length ;i++){
-    console.log("arguments example : ", args[i]);
-}
+
+
+// let args = process.argv;
+// for(let i =2 ; i<args.length ;i++){
+//     console.log("arguments example : ", args[i]);
+// }
+
+
+const methods = require("./math");
+console.log(methods);
+console.log("sum of two numbers : ", methods.sum(8,9));
+console.log("multiplication of two numbers : ", methods.mul(8,8));
