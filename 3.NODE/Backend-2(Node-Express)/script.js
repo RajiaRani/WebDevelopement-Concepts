78a37b59cba338801f6ad5fa0b4d910ad5133ee4
Rajia Rani <rajiarani.syal@gmail.com>
2024-03-28 22:44:24 -0500

    added

---
+++ 3.NODE/Backend-2(Node-Express)/script.js
@@ -0,0 +1,10 @@
+/* LIBRARY */
+/* A library is a collection of pre-written code that can be used to perform specific tasks. eg. axios */
+/* Framework  */
+/* 
+   A framework is a set of pre-written code that provides a structure software applications. eg. express
+   1. express is listen the request.
+   2. after listen passe the request. 
+   3. To match response wiith route.
+   4. how to send suitable response according to request.
+ */
