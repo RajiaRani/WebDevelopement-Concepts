aae8e8492cd0ada08958aa6bd6664e263cef7bd6
Rajia Rani <rajiarani.syal@gmail.com>
2024-03-29 17:40:22 -0500

    added

--- MiniProject/index.js
+++ MiniProject/index.js
@@ -8,6 +8,18 @@ app.use(express.static("public")); // join the style files
 app.use(express.static(path.join(__dirname, "/public/css")));
 app.use(express.static(path.join(__dirname, "/public/js")));
 
+app.get("/portfolio/:user", (req,res) => {
+    let {user} = req.params;
+    const userData = require("./data.json");
+    let data = userData[user];
+   // console.log(data);
+    if(!data) {
+        res.render("error.ejs");
+    } else {
+        res.render("home.ejs", {data});
+    }
+});
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
