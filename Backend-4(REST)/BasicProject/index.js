c72050af875a4a56fa0470bb5179d6279dc1a478
Rajia Rani <rajiarani.syal@gmail.com>
2024-04-01 11:55:16 -0500

    added

--- Backend-4(REST)/BasicProject/index.js
+++ Backend-4(REST)/BasicProject/index.js
@@ -3,25 +3,28 @@ const app = express();
 const path = require("path");
 const { v4: uuidv4 } = require("uuid");// to create the id
 
-const fs = require("fs"); // for json data
-
 app.use(express.urlencoded({ extended:true }));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
-let posts = [];
-// Load existing posts from data.json if it exists
-if (fs.existsSync('./data.json')) {
-    posts = require('./data.json');
-} else {
-    // If data.json doesn't exist, initialize posts as an empty array
-    fs.writeFileSync('./data.json', JSON.stringify(posts)); // Create an empty data.json file
-}
+
+let posts = [
+    {
+        username: "rajiarani",
+        id: uuidv4(),
+        content: "Hello, i am rajia rani, i am assistant professor in Chemistry."
+      },
+      {
+        username: "ChenchuReddy",
+        id: uuidv4(),
+        content: "Hello, i am chenchu reddy, i am Senior Mananger in Byjus."
+      }
+];
+
 
 app.get("/posts", (req,res) => {
-    const posts = require("./data.json");
     res.render("index.ejs", {posts});
 });
 
@@ -31,16 +34,14 @@ app.get("/posts/new", (req,res) => {
 
 app.post("/posts", (req,res) => {
     //console.log(req.body);
-    const posts = require("./data.json");
     let {username, content} = req.body;
-    posts.push({username, content});
-    fs.writeFileSync("./data.json", JSON.stringify(posts)); // for working the push operations
+    let id = uuidv4();
+    posts.push({username, content, id});
     // res.send("post req working.")
     res.redirect("/posts");
 });
 
 app.get("/posts/:id", (req,res) => {
-    const posts = require("./data.json");
     let { id } = req.params;
     //console.log(id);
     let post = posts.find((p) => id === p.id);
