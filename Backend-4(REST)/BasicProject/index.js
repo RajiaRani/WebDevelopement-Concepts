da72ddac898ff2c749321e7c62669240f5b493fa
Rajia Rani <rajiarani.syal@gmail.com>
2024-04-01 13:00:12 -0500

    added

--- Backend-4(REST)/BasicProject/index.js
+++ Backend-4(REST)/BasicProject/index.js
@@ -2,8 +2,11 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const { v4: uuidv4 } = require("uuid");// to create the id
+const methodOverride = require('method-override')
 
 app.use(express.urlencoded({ extended:true }));
+app.use(methodOverride('_method')); // override with POST having ?_method=DELETE
+
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -48,6 +51,24 @@ app.get("/posts/:id", (req,res) => {
     //console.log(post);
     //res.send("done");
     res.render("show.ejs", {post});
+});
+
+app.patch("/posts/:id", (req,res) => {
+    let { id } = req.params;
+    //console.log(id);
+    let newContent = req.body.content;
+    //console.log(newContent);
+     let post = posts.find((p) => id === p.id);
+     post.content = newContent;
+     console.log(post);
+    //res.send("patch request.")
+    res.redirect("/posts");
+});
+
+app.get("/posts/:id/edit", (req,res) => {
+    let { id } = req.params;
+    let post = posts.find((p) => id === p.id );
+    res.render("edit.ejs", {post});
 });
 
 app.listen(3000, () => {
