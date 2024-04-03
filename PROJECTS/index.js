9135ac8f94c495ade65f80eabbe7fa4d03575126
Rajia Rani <rajiarani.syal@gmail.com>
2024-04-02 20:18:34 -0500

    added

--- PROJECTS/index.js
+++ PROJECTS/index.js
@@ -10,15 +10,66 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
+/* Data is defining here */
+let usersData = [
+    {
+        id: uuidv4(),
+        username: "rajia rani",
+        email: "rajia@gmail.com",
+        job: "Assistant Professor",
+        gender: "female",
+        study: "MSc(Chemisrty)",
+        img: "https://images.unsplash.com/photo-1485875437342-9b39470b3d95?q=80&w=2073&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    }
+];
+let aboutData =  [
+    {
+        id: uuidv4(),
+        username: "rajia rani",
+        email: "rajia@gmail.com",
+        website:"www.domain.com",
+        phone:"836*******71",
+        study: "MSc(Chemistry)",
+        insitute: "Lovely Proffessional University. Punjab",
+        mark: "62%",
+        year: "2017-2019",
+        project:"Synthesis of BIM"
+    }
+];
+let contentsData =[
+    {
+        id: uuidv4(),
+        title: "Topic HTML",
+        subject: "html,css",
+        des: "here i am writing the subject related information.here i am writing the subject related information here i am writing the subject related information here i am writing the subject related information here i am writing the subject related information here i am writing the subject related information"
+    }
+];
+let projectsData = [
+    {
+        id: uuidv4(),
+        title: "Mini Project using HTML and CSS",
+        des: "Mini Project decription heree!!",
+        skill: "HTML , CSS"
+    }
+];
+let courseData = [
+    {
+        id: uuidv4(),
+        course_name:"Full stack",
+        course_des:"My course little description",
+        date:"14 02 2022"
+    }
+];
+let skillsData = [
+    { 
+     id: uuidv4(),
+     skill:"html"
+   }
 
-const usersData = require("./json/user.json");
-const aboutData = require("./json/about.json");
-const contentsData = require("./json/content.json");
-const projectsData = require("./json/projects.json");
-const skillsData = require("./json/skill.json");
-const courseData = require("./json/course.json");
+];
 
-const fs = require('fs');
+
+
 
 // Read the JSON file
 
@@ -50,17 +101,18 @@ app.post("/resume/content", (req,res) => {
 });
 
 // For skills container
-app.get("/resume/:id", (req,res) => {
+app.get("/resume/skills/:id", (req,res) => {
    let { id } = req.params;
    let skill = skillsData.find((skill) => id === skill.id);
    console.log(skill);
    res.render("skill.ejs", {skill});
   
 });
-app.post("/resume/skill", (req,res) => {
+app.post("/resume", (req,res) => {
    let { id, skill} = req.body;
    skillsData.push({skill, id});
-   console.log(skillsData);
+   //console.log(skillsData);
+   res.redirect("/resume");
 });
 
 app.get("/resume/:id", (req,res) => {
