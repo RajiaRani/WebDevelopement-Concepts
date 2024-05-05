54f761548a40399067ade16833baf671cac4f9e7
Rajia Rani <rajiarani.syal@gmail.com>
2024-05-05 06:20:03 +0530

    added

--- 7.MongoDB/mongodbwithExp/index.js
+++ 7.MongoDB/mongodbwithExp/index.js
@@ -1,5 +1,9 @@
-const app = require("express");
+const express = require("express");
 const app = express();
+
+app.get("/", (req,res) => {
+    res.send("Hello, i am root node");
+})
 
 app.listen(8080, () => {
     console.log("Hii, I am listening  on port 8080.");
