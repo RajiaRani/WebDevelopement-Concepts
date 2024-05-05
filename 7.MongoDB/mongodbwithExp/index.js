69f1f40e1c67364b87b55396a157041df7ef7c9c
Rajia Rani <rajiarani.syal@gmail.com>
2024-05-05 06:37:36 +0530

    added

--- 7.MongoDB/mongodbwithExp/index.js
+++ 7.MongoDB/mongodbwithExp/index.js
@@ -1,5 +1,16 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+main()
+.then(() => {
+    console.log("connection is successfull");
+}).catch((err) => {
+    console.log(err);
+});
+
+async function main() {
+    await mongoose.connect("ongodb://127.0.0.1:27017/chatapp");
+};
 
 app.get("/", (req,res) => {
     res.send("Hello, i am root node");
