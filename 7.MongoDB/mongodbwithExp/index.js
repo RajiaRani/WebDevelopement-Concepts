232e752cdd5e3c828fc62f1de2baa45888514cb3
Rajia Rani <rajiarani.syal@gmail.com>
2024-05-05 07:51:12 +0530

    added new files

--- 7.MongoDB/mongodbwithExp/index.js
+++ 7.MongoDB/mongodbwithExp/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
+const Chat = require("./models/chats.js");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view  engine", "ejs");
