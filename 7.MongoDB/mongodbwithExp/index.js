2d3f383bab567e66250991f17a2cd13b7382ad4a
Rajia Rani <rajiarani.syal@gmail.com>
2024-05-19 06:56:32 +0530

    added edit route

--- 7.MongoDB/mongodbwithExp/index.js
+++ 7.MongoDB/mongodbwithExp/index.js
@@ -60,7 +60,7 @@ app.post("/chats", (req,res) => {
 // Edit Route
 app.get("/chats/:id/edit", async(req,res) => {
     let { id } = req.params;
-    let chat = await Chat.findById(id);
+    let chat = await Chat.findById(id); // find the chat based on id and saved in chat.
     res.render("edit.ejs", {chat});
 });
 
