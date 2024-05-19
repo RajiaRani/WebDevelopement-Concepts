bd9479d9368c65d98079644ab72fe5e0542ba3d3
Rajia Rani <rajiarani.syal@gmail.com>
2024-05-19 08:41:41 +0530

    added

--- Project2/index.js
+++ Project2/index.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const app = express();
+const mongoose = require('mongoose');
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/test');
+}
 
 //HOME ROUTE
 app.get("/", (req,res) => {
