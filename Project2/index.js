0e4ab134a3e40fe532bb709ed9c6de022b4fe322
Rajia Rani <rajiarani.syal@gmail.com>
2024-05-20 10:04:10 +0530

    added

--- Project2/index.js
+++ Project2/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+
 
 main()
 .then( res => console.log("connection is done"))
