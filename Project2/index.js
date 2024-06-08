f51b22bd626924a18210e87fdd195c43cf1abb19
Rajia Rani <rajiarani.syal@gmail.com>
2024-06-08 08:48:05 +0530

    added

--- Project2/index.js
+++ Project2/index.js
@@ -15,8 +15,6 @@ async function main() {
 app.get("/", (req,res) => {
     res.send("Home Root is working");
 });
-
-
 // PORT LISTENING
 app.listen(8080, () => {
     console.log("server is listening on port 8080.");
