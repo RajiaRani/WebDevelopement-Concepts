d4cce2a6ea903507e9974ce7c937260af3b1c089
Rajia Rani <rajiarani.syal@gmail.com>
2024-06-22 05:09:57 -0500

    added

--- Project2/index.js
+++ Project2/index.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose"); // require mongoose
 
 main()
 .then( res => console.log("connection is done"))
-.catch(err => console.log(err));
+.catch( (err) => console.log(err));
 
 async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/resumeDemo");
