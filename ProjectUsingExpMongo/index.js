0afeb17b849329c1ab5d922ff7228b730c99ece3
Rajia Rani <rajiarani.syal@gmail.com>
2024-06-22 07:05:51 -0500

    added

--- ProjectUsingExpMongo/index.js
+++ ProjectUsingExpMongo/index.js
@@ -1,4 +1,11 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://127.0.0.1:27017/studentData");
+
+main()
+.then((res) => { console.log("working")})
+.catch((err) => console.log(err));
+
+async function main() {
+    await mongoose.connect("mongodb://127.0.0.1:27017/studentData");
+};
