5af190418758fcf43fd9fe548dc51057bc127fea
Rajia Rani <rajiarani.syal@gmail.com>
2024-06-22 07:44:30 -0500

    added

--- ProjectUsingExpMongo/index.js
+++ ProjectUsingExpMongo/index.js
@@ -9,3 +9,19 @@ main()
 async function main() {
     await mongoose.connect("mongodb://127.0.0.1:27017/studentData");
 };
+
+// create the schema
+const studentSchema = new mongoose.Schema({
+    name : String,
+    class : Number,
+    section : String,
+    rollno: Number,
+    result:String
+});
+
+const Student = mongoose.model("Student", studentSchema);
+const std1 = new Student({name: "Rajia Rani", class: 10, section:"A", rollno:12456, result:"Pass"});
+//std1.save();
+const std2 = new Student({name: "Priya Sharma", class: 12, section:"A", rollno:12496, result:"Pass"});
+//std2.save();
+
