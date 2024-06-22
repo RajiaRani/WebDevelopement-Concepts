4ec17103e78745256519909e1de8ff4bcb6339e1
Rajia Rani <rajiarani.syal@gmail.com>
2024-06-22 04:52:40 -0500

    added

--- Project2/index.js
+++ Project2/index.js
@@ -9,12 +9,15 @@ main()
 
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/resumeDemo');
-}
+};
+
 
 //HOME ROUTE
 app.get("/", (req,res) => {
     res.send("Home Root is working");
 });
+
+
 // PORT LISTENING
 app.listen(8080, () => {
     console.log("server is listening on port 8080.");
