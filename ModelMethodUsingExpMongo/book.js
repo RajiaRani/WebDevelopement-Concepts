97fa3e234b07e025fb1ea5635d3c1139c0f6bb5e
Rajia Rani <rajiarani.syal@gmail.com>
2024-06-25 06:27:59 -0500

    added demoAmazone

--- ModelMethodUsingExpMongo/book.js
+++ ModelMethodUsingExpMongo/book.js
@@ -8,4 +8,23 @@ main()
 });
 async function main() {
     await mongoose.connect("mongodb://127.0.0.1:27017/demoAmazone");
-}
+};
+
+const bookSchema = new mongoose.Schema({
+    title: {
+        type: String,
+    },
+    author: {
+        type: String,
+        ed: String,
+        age: Number,
+    },
+    price:{
+        type: Number,
+    },
+    languages:{
+        type: String,
+    }
+});
+
+const Book = mongoose.model("Book", bookSchema);
