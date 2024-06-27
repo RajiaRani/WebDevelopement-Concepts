2f43e91e7a6df92b22be56d21ec15b914ba7e560
Rajia Rani <rajiarani.syal@gmail.com>
2024-06-27 11:51:13 -0500

    added

---
+++ 3.NODE/Backend-1(NodeJS)/1.Module/Fruits/index.js
@@ -0,0 +1,6 @@
+const apple = require("./apple");
+const banana = require("./banana");
+const apple = require("./mango");
+
+let fruits = [ apple, banana, mango];
+module.exports = fruits;
---
+++ 3.NODE/Backend-1(NodeJS)/1.Module/script.js
@@ -0,0 +1,16 @@
+/* We can't selectively load only the piece we need with require but with IMPORT , we can selectively load only the pieces we need, which can save memory.*/
+
+// import {sum, pi, div} from "./math.js";
+// console.log("Sum of two numbers is : ", sum(9,90));
+// console.log("Value of Pi : ", pi);
+// console.log("Dividing two numbers : ", div(90,89));
+
+// import { generate } from "random-words";
+
+// console.log("Generate random word :" , generate());
+
+// const method = require("./math");
+// console.log(method.sum(2,2));
+
+const fruitInfo = require("./Fruits");
+console.log(fruitInfo);
