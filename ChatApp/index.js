7dd5de717cbcdb344e4cff35467bf561b6bc07ca
Rajia Rani <rajiarani.syal@gmail.com>
2024-07-17 09:55:25 -0500

    added new chatapp

---
+++ ChatApp/index.js
@@ -0,0 +1,83 @@
+const express = require ("express");
+const app = express();
+const path = require("path");
+const mongoose = require ("mongoose");
+const Chat = require("./models/chat.js");
+const ejsMate = require("ejs-mate");
+const methodOverride = require("method-override");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({extended: true}));
+app.engine("ejs", ejsMate);
+app.use(methodOverride("_method"));
+
+main()
+.then(() => {
+    console.log("connected to DB successfully");
+})
+.catch((err) => {
+    console.log(err);
+});
+
+async function main() {
+    await mongoose.connect("mongodb://127.0.0.1:27017/chatapp")
+}
+
+//Index Route
+app.get("/chats",async (req,res) => {
+let  allChat = await Chat.find({});
+  res.render("chats/index.ejs", {allChat});
+});
+
+//Edit Route
+app.get("/chats/:id/edit", async(req,res) => {
+  let {id} =  req.params;
+  let chat = await Chat.findById(id);
+  //console.log(id);
+  res.render("chats/edit.ejs", {chat} );
+});
+
+//Update Route
+app.put("/chats/:id", async(req,res) => {
+    let { id } = req.params;
+    let { message : msg } = req.body;
+    let updatedChat = await Chat.findByIdAndUpdate(
+    id,
+    { message : msg},
+    {runValidators : true, new: true });
+    //console.log(updatedChat);
+    res.redirect("/chats");
+});
+
+//New Route
+app.get("/chats/new", (req,res) => {
+  res.render("chats/new.ejs");
+});
+app.post("/chats", (req,res) => {
+let { from, to, message } = req.body;
+const newChat = new Chat({
+    from:from,
+    to:to,
+    message:message
+}).save()
+.then((res) => {
+    console.log(res);
+}).catch((err) => {
+    console.log(err);
+});
+res.redirect("/chats");
+});
+
+//Delete Route
+app.delete("/chats/:id", async(req,res) => {
+  let { id } = req.params;
+  let deletedChat = await Chat.findByIdAndDelete(id);
+  //console.log(deletedChat);
+  res.redirect("/chats");
+});
+
+app.listen(8080, () => {
+    console.log("I am listening on port 8080.");
+});
---
+++ ChatApp/init.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+const Chat = require("./models/chat.js");
+
+main()
+.then(() => {
+    console.log("connection successfull");
+}) .catch((err) => {
+    console.log(err);
+});
+
+async function main() {
+    await mongoose.connect("mongodb://127.0.0.1:27017/chatapp");
+}
+
+
+let allchats = [
+    {
+    from: "rajia",
+    to: "roma",
+    message: "Hello, I am rajia here",
+    created_at: new Date()
+   },
+   {
+    from: "roma",
+    to: "laddi",
+    message: "Hello, where are you",
+    created_at: new Date()
+   },
+   {
+    from: "chenchu",
+    to: "rajia",
+    message: "Hello, rajia how are you!",
+    created_at: new Date()
+   },
+   {
+    from: "reetu",
+    to: "pradeep",
+    message: "How are you.",
+    created_at: new Date()
+   },
+   {
+    from: "laddi",
+    to: "rajia",
+    message: "dont msg me.",
+    created_at: new Date()
+   },
+]
+//fill the chat
+Chat.insertMany(allchats);
